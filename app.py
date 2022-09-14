530c3632b35855073913cc6110f7d9f02b702ca0
Gigatronhertz <99260886+Gigatronhertz@users.noreply.github.com>
2022-09-14 10:25:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,24 @@
-from flask import Flask,render_template,request,jsonify
-
+from django.shortcuts import render
+from django.http import HttpResponse
 from mabi_main import get_response
 
 
-app = Flask(__name__)
 
-@app.get("/")
-def index_get():
-    return render_template("base.html")
-@app.post("/predict")
-def predict():
-    text = request.get_json().get("message")
 
-    response = get_response(text)
-    message = {"answer":response}
-    return jsonify(message)
+
+
+def index_get(request):
+    if request.method == 'GET':
+
+        return render (request,"base.html")
+
+def predict(request):
+    if request.method == 'POST':
+        text = request.json("message")
+
+        response = get_response(text)
+        message = {"answer":response}
+        return render(message)
 
 if __name__ == "__main__":
     app.run(debug=True)
